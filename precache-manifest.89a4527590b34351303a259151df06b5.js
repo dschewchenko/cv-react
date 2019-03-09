25fc8b144a6cdcc2fbda148ba4ec8fec3f1a2300
dschewchenko <dschewchenko@keepsolid.com>
2019-03-09 20:40:46 +0200

    Updates

---
+++ precache-manifest.89a4527590b34351303a259151df06b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f325a44d1f9b2dbc375d7567066f5e40",
+    "url": "/cv-react/static/media/ava.f325a44d.jpg"
+  },
+  {
+    "revision": "24824188e9816044171a",
+    "url": "/cv-react/static/js/runtime~main.a326f71c.js"
+  },
+  {
+    "revision": "d87abd2017840202c0d7",
+    "url": "/cv-react/static/js/main.74cf6dcb.chunk.js"
+  },
+  {
+    "revision": "8632ba2820f336f02f0a",
+    "url": "/cv-react/static/js/2.b1876bd2.chunk.js"
+  },
+  {
+    "revision": "d87abd2017840202c0d7",
+    "url": "/cv-react/static/css/main.6ce41ad1.chunk.css"
+  },
+  {
+    "revision": "50b328ea9a490035165f2d1cb69a12d6",
+    "url": "/cv-react/index.html"
+  }
+];
